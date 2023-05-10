bfbd8410c8567eec11f67ddac676fea0cef67d77
Erick Stev ESGM <esgomez2@espe.edu.ec>
2023-05-10 12:21:29 -0500

    Start Proyects

--- POO_P1_PRACT3_OPERADORES_RELACIONALES/src/poo_p1_pract3_operadores_relacionales/POO_P1_PRACT3_OPERADORES_RELACIONALES.java
+++ POO_P1_PRACT3_OPERADORES_RELACIONALES/src/poo_p1_pract3_operadores_relacionales/POO_P1_PRACT3_OPERADORES_RELACIONALES.java
@@ -4,7 +4,7 @@ public class POO_P1_PRACT3_OPERADORES_RELACIONALES {
     public static void main(String[] args) {
         int val1 = 2;
         int val2 = 3;
-        System.out.println("IGUAL" + val1 == val2);
+        System.out.println(val1 == val2);
         System.out.println(val1 != val2);
         System.out.println(val1 > val2);
         System.out.println(val1 < val2);
---
+++ POO_P1_PRACT4_LOGICOS/src/poo_p1_pract4_logicos/POO_P1_PRACT4_LOGICOS.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poo_p1_pract4_logicos;
+
+/**
+ *
+ * @author erick
+ */
+public class POO_P1_PRACT4_LOGICOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int val1 = 7; int val2 = 2;
+        System.out.println((val1 == val2) && (val1 != val2));
+        System.out.println((val1 > val2) || (val2 < val1));
+        System.out.println ((val1 != val2) && (val2 > val1));
+    }
+    
+}
